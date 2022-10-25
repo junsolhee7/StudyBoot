7636d612882cd27abcc45534aaf8ec95030a0338
junsolhee7@naver.com <junsolhee7@naver.com>
2022-10-25 17:45:00 +0900

    1

--- src/main/java/com/iu/home/member/MemberService.java
+++ src/main/java/com/iu/home/member/MemberService.java
@@ -1,5 +1,10 @@
 package com.iu.home.member;
 
-public class MemberService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class MemberService {
+	
 }
